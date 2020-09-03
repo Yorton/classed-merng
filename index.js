72488d300240393983f44daf96a7f97251c9851d
unknown <cruise2955@yahoo.com.tw>
2020-09-03 15:01:19 +0800

    update client path

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('client/public'));
 app.get('*', (req, res) => {
 
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/public', 'index.html'));
 });
 
 const pubsub = new PubSub();
